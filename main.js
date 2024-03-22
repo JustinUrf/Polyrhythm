d7b9e8864cd7119326e429461a0513d7936eb0ad
Justin Lee <lee.justin001126@gmail.com>
2024-03-21 17:52:25 -0700

    moving along a circle, calculating amount of time pass through request animation frames

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import './style.css'
 
+const paper = document.getElementById("paper"),
+      pen = paper.getContext("2d");
+      
 
-const paper = document.querySelector("#paper"),
- pen = paper.getContext("2d");
+let startTime = new Date().getTime();
 
- const draw = () => {
+const draw = () => {
+
+    const currentTime = new Date().getTime(),
+          elapsedTime = (currentTime - startTime) / 1000;
+
     paper.width = paper.clientWidth;
     paper.height = paper.clientHeight;
 
@@ -36,11 +42,24 @@ const paper = document.querySelector("#paper"),
     pen.arc(center.x, center.y, length * 0.05, Math.PI, 2 * Math.PI);
     pen.stroke();
 
+    const velocity = length * 0.05,
+          maxAngle = 2 * Math.PI,
+          distance = Math.PI +(elapsedTime * velocity),
+          modDistance = distance % maxAngle,
+          adjustedDistance = modDistance >= Math.PI ? modDistance : maxAngle - modDistance;
+
+    const x = center.x + arcRadius * Math.cos(adjustedDistance),
+          y = center.y +arcRadius * Math.sin(adjustedDistance);
+
     //Draw the circle
+    pen.fillStyl = "white";
     pen.beginPath();
     pen.arc(center.x, center.y, length * 0.0065, 0, 2 * Math.PI);
+    pen.fill();
+
+    requestAnimationFrame(draw);
+
  }
 
- init();
 
  draw();
